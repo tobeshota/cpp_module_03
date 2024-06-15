48a63a75cadab1a22880e45bcbfb19769555cb0b
tobeshota <Shota.Tobe@keio.jp>
2024-06-15 12:37:05 +0900

    [fix] fix test cord in ex03

--- ex03/test.sh
+++ ex03/test.sh
@@ -77,39 +77,24 @@ function run_test {
 	printf "|- - - - - - - - - - start - - - - - - - - - -|\n"
 	# 追加形式: assert <actual> <expect>
 	assert "" "- 0 ────────────────────
-(constructor)ClapTrap FT_1 has been created!
-(constructor)FragTrap FT_1 has been created!
-- 1 ────────────────────
-ClapTrap FT_1 attacks target1, causing 30 points of damage!
-- 2 ────────────────────
-ClapTrap FT_1 takes 5 points of damage!
-- 3 ────────────────────
-ClapTrap FT_1 is repaired for 3 points!
-- 4 ────────────────────
-FragTrap FT_1 requests a high five!
-- 5 ────────────────────
-(constructor)ClapTrap Copy constructor called
-ClapTrap Copy assignment operator called
-(constructor)FragTrap Copy constructor called
-FragTrap Copy assignment operator called
-ClapTrap Copy assignment operator called
-- 6 ────────────────────
-FragTrap FT_1 requests a high five!
-- 7 ────────────────────
-(constructor)ClapTrap Default constructor called
+(constructor)ClapTrap Diamond_clap_name has been created!
 (constructor)FragTrap Default constructor called
-- 8 ────────────────────
-FragTrap Copy assignment operator called
-ClapTrap Copy assignment operator called
-- 9 ────────────────────
-FragTrap FT_1 requests a high five!
-- 10 ────────────────────
-(destructor)FragTrap FT_1 has been destroyed!
-(destructor)ClapTrap FT_1 has been destroyed!
-(destructor)FragTrap FT_1 has been destroyed!
-(destructor)ClapTrap FT_1 has been destroyed!
-(destructor)FragTrap FT_1 has been destroyed!
-(destructor)ClapTrap FT_1 has been destroyed!"
+(constructor)ScavTrap Default constructor called
+(constructor)DiamondTrap Diamond has been created!
+- 1 ────────────────────
+ScavTrap Diamond_clap_name attacks target, causing 20 points of damage!
+- 2 ────────────────────
+ClapTrap Diamond_clap_name takes 20 points of damage!
+- 3 ────────────────────
+ClapTrap Diamond_clap_name is repaired for 10 points!
+- 4 ────────────────────
+I am Diamond and my ClapTrap name is Diamond_clap_name
+DiamondTrap Diamond is now in Gate keeper mode!
+- 5 ────────────────────
+(destructor)DiamondTrap Diamond has been destroyed!
+(destructor)ScavTrap Diamond_clap_name has been destroyed!
+(destructor)FragTrap Diamond_clap_name has been destroyed!
+(destructor)ClapTrap Diamond_clap_name has been destroyed!"
 }
 
 # - main ---------------------------
